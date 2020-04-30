45d22252cc0f7e0618696bb0ece7d68e52c67f62
StefaniaHaiduc <64540654+StefaniaHaiduc@users.noreply.github.com>
2020-04-30 17:14:54 +0300

    Login Page

---
+++ ReservEatLogin/ReservEatLogin/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReservEatLogin
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ReservEatLogin/ReservEatLogin/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReservEatLogin
+{
+    class Main
+    {
+        Form1 frm1 = new Form1();
+        Form2 frm2 = new Form2();
+        Form3 frm3 = new Form3();
+    }
+}
